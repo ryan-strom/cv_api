867c75f474f84742470ed13db201dfb17ff33915
ryanstromberg <rstromberg916@gmail.com>
2016-05-06 15:30:27 -0400

    fixing wsgi

--- cv_api/wsgi.py
+++ cv_api/wsgi.py
@@ -6,8 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cv_api.settings")
 
 from django.core.wsgi import get_wsgi_application
