8c0dbf9a62b02a84106734face8b6cde1731f6b2
ryanstromberg <rstromberg916@gmail.com>
2016-05-06 18:36:56 -0400

    error handling on wsqi

--- cv_api/wsgi.py
+++ cv_api/wsgi.py
@@ -17,6 +17,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 import sys
+import time
+import traceback
+import signal
 
 sys.path.insert(0, '/opt/python/current/app')
 sys.path.append('/usr/local/lib/python2.7/site-packages')
@@ -24,4 +27,13 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'cv_api.settings'
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+    print 'WSGI without exception'
+except Exception:
+    print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
